b141b4beca81dac6b09ef85c6687ef67971341d9
kazuma1989 <funifuni.1204@gmail.com>
2017-12-29 23:21:27 +0900

    コントローラーのサンプルを追加

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,14 +1,29 @@
 'use strict';
 
+class BA0010UA {
+
+  constructor() {
+    this.text = 'I`m View1';
+    this.newText = '';
+  }
+
+  send() {
+    console.log('send called!!!', this.text);
+  }
+
+  change() {
+    this.text = this.newText;
+  }
+
+}
+
 angular.module('myApp.view1', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
-    templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
-  });
-}])
+  .config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/view1', {
+      templateUrl: 'view1/view1.html',
+      controller: 'BA0010UA'
+    });
+  }])
 
-.controller('View1Ctrl', [function() {
-
-}]);
+  .controller('BA0010UA', [BA0010UA]);
